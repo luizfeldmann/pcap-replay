3e8e78eb8db3d6a0ec233250c7bbb7d278abd0a3
Luiz Gustavo Pfitscher e Feldmann <lfeldmann@ael.com.br>
2026-02-03 20:20:13 +0400

    Add linter and prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,76 @@
+const { defineConfig } = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const prettier = require("eslint-plugin-prettier");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: [
+          "./packages/server/tsconfig.json",
+          "./packages/shared/tsconfig.json",
+          "./packages/ui/tsconfig.json",
+        ],
+
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      prettier,
+    },
+
+    extends: compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:prettier/recommended",
+    ),
+
+    rules: {
+      "prettier/prettier": "error",
+      "no-return-await": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/require-await": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-non-null-assertion": "error",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: [
+            "./packages/shared/tsconfig.json",
+            "./packages/server/tsconfig.json",
+            "./packages/ui/tsconfig.json",
+          ],
+        },
+      },
+    },
+  },
+]);
---
+++ packages/ui/eslint.config.cjs
@@ -0,0 +1,24 @@
+const { defineConfig } = require("eslint/config");
+
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    extends: compat.extends("../../.eslintrc.cjs"),
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+]);
